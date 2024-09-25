a392206784d1a0882f789f6f03b1b897954ff6e3
Abylaikhan <157004674+RiceEnjoyer21@users.noreply.github.com>
2024-09-25 10:09:55 +0500

    Add files via upload

---
+++ lab1.sql
@@ -0,0 +1,27 @@
+create database lab1;
+
+create table users
+(
+    id        serial,
+    firstname varchar(50),
+    lastname  varchar(50)
+);
+
+alter table users add column isadmin integer;
+
+alter table users alter column isadmin type boolean using isadmin::boolean;
+
+alter table users alter column isadmin set default false;
+
+alter table users add constraint user_id primary key(id);
+
+create table tasks
+(
+    id serial,
+    name varchar(50),
+    user_id integer
+);
+
+drop table tasks;
+
+drop database lab1;
