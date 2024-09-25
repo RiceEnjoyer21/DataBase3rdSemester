2b32141b5d37ad14bc275ee465b181b34b9b00fb
Abylaikhan <157004674+RiceEnjoyer21@users.noreply.github.com>
2024-09-25 16:05:38 +0500

    Add files via upload

---
+++ lab2.sql
@@ -0,0 +1,52 @@
+create database lab2;
+
+create table countries
+(
+    country_id serial primary key,
+    country_name varchar(50),
+    region_id integer,
+    population integer
+);
+
+insert into countries (country_name, region_id, population)
+values ('Japan', 21, 20000000);
+
+insert into countries (country_id, country_name)
+values (1, 'Germany');
+
+insert into countries (region_id)
+values (null);
+
+insert into countries (country_name, region_id, population)
+values ('Belgium', 7, 300000000),
+       ('Slovenia', 8, 400000000),
+       ('Albania', 9, 5000000);
+
+alter table countries alter column country_name set default 'Kazakhstan';
+
+insert into countries(country_name, region_id, population) values (default, 91, 100000000);
+
+insert into countries default values;
+
+create table countries_new (like countries including all);
+
+insert into countries_new select * from countries;
+
+update countries_new set region_id = 1 where region_id is null;
+
+update countries_new set population = population * 1.1
+returning country_name, population as new_population;
+
+delete from countries where population < 100000;
+
+delete from countries_new using countries where countries_new.country_id = countries.country_id returning countries_new.*;
+
+delete from countries returning *;
+
+
+
+
+
+
+
+
